03fa46c58bde73f46ce5b1d5e24b3032ce8b483a
andresbluna <andres.benjamin.luna@gmail.com>
2024-11-23 00:07:22 -0300

    update changes to controller ventas

--- src/main/java/com/vmark/pos/repository/VentaRepository.java
+++ src/main/java/com/vmark/pos/repository/VentaRepository.java
@@ -3,16 +3,25 @@ package com.vmark.pos.repository;
 import com.vmark.pos.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
 
 public interface VentaRepository extends JpaRepository<Venta, Long> {
-    List<Venta> findByFechaVentaAfter(Date fecha);
 
     @Query(value = "SELECT calcular_promedio_semanal() FROM DUAL", nativeQuery = true)
     BigDecimal obtenerPromedioSemanal();
+
+    @Procedure(name = "crear_venta")
+    BigDecimal crearVenta(
+            @Param("p_fecha_venta") Date fechaVenta,
+            @Param("p_monto_total") BigDecimal montoTotal,
+            @Param("p_metodo_pago") String metodoPago,
+            @Param("p_empleado_id") Long empleadoId
+    );
+
+    
 
 }
